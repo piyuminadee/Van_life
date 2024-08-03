a2f26c9bf7745630a04931fd41f0d27ddc261e06
Nadeesh Weerarathna <nadeesh-cs20024@stu.kln.ac.lk>
2024-08-03 19:41:22 +0530

    Nesting Host routes

--- index.jsx
+++ index.jsx
@@ -6,22 +6,30 @@ import About from "./pages/About"
 import Vans from './pages/Vans';
 import './server'
 import VanDetails from './pages/VanDetails';
+import Layers from './component/Layers';
+import Dashboard from './pages/Host/Dashboard';
+import Reviews from './pages/Host/Reviews';
+import Income from './pages/Host/Income';
+import HostLayout from './component/HostLayout';
 
 function App() {
   return (
     <BrowserRouter>
-      <header>
-        <Link className="site-logo" to="/">#VanLife</Link>
-        <nav>
-          <Link to="/about">About</Link>
-          <Link to="/vans">Vans</Link>
-        </nav>
-      </header>
+      
       <Routes>
+        <Route element={<Layers />}>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/vans" element={<Vans /> } />
         <Route path='/vans/:id' element={<VanDetails />} />
+
+        <Route element={<HostLayout />}>
+        <Route path="/host" element={<Dashboard />} />
+        <Route path="/host/reviews" element={<Reviews />} />
+        <Route path="/host/income" element={<Income />} />
+        
+        </Route>
+        </Route>
       </Routes>
     </BrowserRouter>
   )
